52a73c623c6fad84456cbcdde54afcdd7e751c45
Dan Kral <dankral01@gmail.com>
2023-08-01 20:14:53 -0400

    add jest and example test

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-// you can use 'import' syntax!
+// you can use import export syntax!
 
-import fs from "fs";
-
-fs.writeFileSync("hello.txt", "Hello World!");
-const res = fs.readFileSync("hello.txt", "utf8");
-
-console.log(res);
-
-fs.unlinkSync("hello.txt");
+export default function doStuff() {
+    return null;
+}
